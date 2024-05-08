6c4f9a18cb4e3d54f8dbad386aab6a613c8cc88a
Nhut Huynh <huynhminhnhut@gmail.com>
2024-05-08 23:08:36 +0700

    feat: integrate with random string

--- automation-framework/e2etests/tests/features/homepage.feature
+++ automation-framework/e2etests/tests/features/homepage.feature
@@ -2,6 +2,6 @@ Feature: to test homepage functionality
 
   Scenario: to test homepage
     Given User is on homepage
-    When User enter login details
-    Then Login should be successful
-    And Home page should be displayed
+    When User at login page
+    # Then Login should be successful
+    # And Home page should be displayed
--- automation-framework/e2etests/tests/steps/homepagesteps.ts
+++ automation-framework/e2etests/tests/steps/homepagesteps.ts
@@ -1,25 +1,10 @@
-import { Given, When, Then } from "@cucumber/cucumber"
-import { Browser, BrowserContext, Page, chromium } from "playwright"
+import { Given, When } from "@cucumber/cucumber";
 
-Given('User is on homepage', { timeout: 2 * 10000 }, async function () {
-    let browser: Browser = await chromium.launch({ headless: false, channel: "chrome", args: ['--start-maximized'] });
-    let bCtx: BrowserContext = await browser.newContext({ viewport: null, javaScriptEnabled: true });
-    let page: Page = await bCtx.newPage();
-    await page.goto('https://ecommerce-playground.lambdatest.io/');
-    await page.close();
-    await browser.close();
+Given(/(.*) is on (.*)/, function (actor: string, page: string) {
+    console.log('Home')
 });
 
-
-When('User enter login details', function () {
-    console.log('-------User is on login details-------')
-});
-
-
-Then('Login should be successful', function () {
-    console.log('-------Login should be successful-------')
-});
-
-Then('Home page should be displayed', function () {
-    console.log('-------Home page should be displayed-------')
+When(/(.*) at (.*) page/, function (actor: string, page: string) {
+    // Step implementation for When User at login page
+    console.log('Login')
 });
